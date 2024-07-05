02f469d32f0d36f6f4b4efab20c780ddb5a9dc08
Jack Le Hamster <jacklehamster@gmail.com>
2024-07-04 23:32:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Package Rename action
+
+This action renames the repo in package.json to match the current repo.
+Useful for github templates of Node projects.
+
+## Inputs
+
+None
+
+## Outputs
+
+None
+
+## Example usage
+
+uses: actions/package-rename-action@v1.06
+
+see [example here](.github/workflows/main.yml)
---
+++ index.cjs
@@ -0,0 +1,33 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require("fs");
+
+try {
+  const fileName = core.getInput('file');
+  const file = fs.readFileSync(fileName);
+  const json = JSON.parse(file);
+
+  const fullRepository = core.getInput('repository');  
+  json.name = fullRepository.split("/")[1];
+  if (typeof json.repository === "string") {
+    json.repository = fullRepository;
+  } else if (json.repository?.url) {
+    json.repository.url = `git+https://github.com/${fullRepository}.git`;
+  }
+  if (json.bugs) {
+    json.bugs.url = `https://github.com/${fullRepository}/issues`;
+  }
+  json.homepage = `https://github.com/${fullRepository}#readme`;
+
+  const newJSON = JSON.stringify(json, null, "   ") + "\n";
+
+  if (file !== newJSON) {
+    fs.writeFileSync(fileName, newJSON);
+  }
+  
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
